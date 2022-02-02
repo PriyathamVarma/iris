3f62a95bbfcc74765ecab984d4d28ad4563fd382
Varma <varma@quickfiredigital.com>
2022-02-02 13:39:25 +0000

    some changes

--- ml_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.conf.urls import include
-from ml_app import views
-
-# add url functions here for views
-urlpatterns = [
-    path('',views.hello_world),
-]
--- ml_project/urls.py
+++ ml_project/urls.py
@@ -20,6 +20,5 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/',include('ml_app.urls')),
     path('prediction/',include('model_app.urls')),
 ]
